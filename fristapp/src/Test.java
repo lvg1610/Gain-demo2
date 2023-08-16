83ecb4905208056413ba7c868b7a9016beb9cd68
lvg1610 <lvg1610@gmail.com>
2023-08-16 15:00:53 +0530

    first java app fristapp is added

---
+++ fristapp/src/Test.java
@@ -0,0 +1,14 @@
+import com.abc.utility.Calculator;
+/*import static com.abc.utility.Calculator.sum;
+import static com.abc.utility.Calculator.PI;*/
+
+import static com.abc.utility.Calculator.*;
+public class Test
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(PI);
+		System.out.println(sum(89,76));
+		System.out.println(power(5,3));
+	}
+}
